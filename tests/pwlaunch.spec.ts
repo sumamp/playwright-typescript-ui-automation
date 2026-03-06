6b5953ce94553b07239df3b35073d2d8c1dce4a7
Suma <sumamp9@gmail.com>
2026-03-06 15:26:13 +0530

    Login test cases

--- tests/pwlaunch.spec.ts
+++ tests/pwlaunch.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Swag Labs');
+    await page.goto('https://www.saucedemo.com/');
+    await expect(page).toHaveTitle('Swag Labs');
+    await page.fill("#user-name",'standard_user');
+    await page.fill("#password",'secret_sauce');
+    await page.click("#login-button");
+    await (expect(page.getByText('Swag Labs'))).toBeVisible();
 });
 
